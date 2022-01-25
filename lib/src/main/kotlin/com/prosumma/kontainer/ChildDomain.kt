82ff491c8efc8b4b4f86cbfedfecb57ad52d2ef7
Gregory Higley <gregory.higley@patrontechnology.com>
2022-01-25 13:28:01 -0500

    Lint

--- lib/src/main/kotlin/com/prosumma/kontainer/ChildDomain.kt
+++ lib/src/main/kotlin/com/prosumma/kontainer/ChildDomain.kt
@@ -1,6 +1,8 @@
 package com.prosumma.kontainer
 
-abstract class ChildDomain<Parent: ParentDomain>(name: Name, val parent: Parent): ParentDomain(name) {
+abstract class ChildDomain<Parent: ParentDomain>(
+    name: Name, val parent: Parent
+): ParentDomain(name) {
     constructor(name: String, parent: Parent): this(Name(name), parent)
 
     final override val container: Container
